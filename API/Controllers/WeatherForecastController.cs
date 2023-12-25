bf939c4e68e840e45947a0f02eddec8658771085
adamkruczkowski <adam.kruczkowsk@gmail.com>
2023-12-25 19:34:41 +0100

    .

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace API.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    string x = "xd";
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
